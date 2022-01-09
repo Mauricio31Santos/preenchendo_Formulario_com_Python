49aaa6ac30853d14afff2d892cb97ac756a609fa
mauricio31santos <mauriciobsantos31@outlook.com>
2022-01-09 00:31:09 -0300

    Preenchendo as informaçoes contidas no excell

--- preenchendo_form.py
+++ preenchendo_form.py
@@ -7,16 +7,54 @@ navegador.get("https://forms.gle/fTG7MkjZqWaRgg2GA")
 
 
 #preenchendo o cpf
-#//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input
+navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys()
 
-navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('32223325521')
 #preencher o email
-navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('mauricio@hot.com')
+navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys()
+
 #preencher descrição
-navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys('Preenchendo as informações com python!!')
+navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys()
+
 #preencher valor
-navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('3120')
+navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys()
 
 #clicar no botão
-navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span').click('enviar')
+navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span').click()
+
+
+## fazer a partir de uma planilha e varias vezes:
+
+import pandas as pd
+from IPython.display import display 
+
+tabela = pd.read_excel('Emitir.xlsx')
+display(tabela)
+
+
+# para cada cpf na planilha, emitir uma nota fiscal
+
+for i,cpf in enumerate(tabela["CPF"]):
+    email = tabela.loc[i, "Email"]
+    descrição = tabela.loc[i, "Descrição"]
+    valor = tabela.loc[i, "Valor"]
+
+    navegador.get("https://forms.gle/fTG7MkjZqWaRgg2GA")
+    
+
+        #preenchendo o cpf
+    navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(cpf)
+
+    #preencher o email
+    navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(email)
+
+    #preencher descrição
+    navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys(descrição)
+
+    #preencher valor
+    navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(str(valor))
+
+    #clicar no botão
+    navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span').click()
+
+        
 
